a504c3c38cab6025a2f985530a6bbbc40d03eb2a
Tiago <tico26402@gmail.com>
2024-06-13 10:48:21 +0100

    padding on the left

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ const router = useRouter();
 <template>
   <div class="h-screen w-screen bg-stone-100">
     <div @click="router.push({ path: '/' })" class="top h-24 w-screen bg-white p-4 fixed shadow-md cursor-pointer">
-      <h1 class="text-6xl font-semibold">Estação Metereológica ESDS</h1>
+      <h1 class="text-6xl font-semibold pl-4">Estação Metereológica ESDS</h1>
     </div>
     <router-view class="pt-24 h-full w-full"/>
   </div>
